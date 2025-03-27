1ffcb25b0083e6c8833173285dae82d3fde3bf9c
Alfonso Rafael Solis Rangel <alfonsorafaelsolisrangel@gmail.com>
2025-03-27 10:03:48 -0600

    Validación si el usuario no esta activo en la plataforma

--- app/middleware/check_roles_middleware.ts
+++ app/middleware/check_roles_middleware.ts
@@ -7,6 +7,8 @@ export default class CheckRolesMiddleware {
 
     if(!user) return ctx.response.unauthorized({message: "Debes iniciar sesión para acceder a este recurso."});
 
+    if(!user.isActive) return ctx.response.unauthorized({message: "Usuario baneado, por favor contacta a administración."});
+
     if(!allowedRoles.includes(user.role)) return ctx.response.forbidden({message: "Acceso denegado, no cuentas con los permisos suficientes para acceder a este recurso."});
 
     return await next();
