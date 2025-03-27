612ea67ca192ee299d561f8851e95c93fe6c4ed1
Alfonso Rafael Solis Rangel <alfonsorafaelsolisrangel@gmail.com>
2025-03-26 23:07:23 -0600

    Se añadio la columna si el usuario esta activo

--- app/models/user.ts
+++ app/models/user.ts
@@ -29,6 +29,9 @@ export default class User extends compose(BaseModel, AuthFinder) {
   @column({ serializeAs: null })
   declare password: string
 
+  @column()
+  declare isActive: boolean
+
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
