37387c22a544fff70bc065d432ca6c2df7cbbcf5
Alfonso Rafael Solis Rangel <alfonsorafaelsolisrangel@gmail.com>
2025-03-27 09:53:53 -0600

    Implementacion de los middlewares en la ruta de realizar un registro de usuario

--- start/routes.ts
+++ start/routes.ts
@@ -9,6 +9,7 @@
 
 import AuthController from '#controllers/auth_controller';
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js';
 
 router.get('/', async () => {
   return {
@@ -18,7 +19,7 @@ router.get('/', async () => {
 
 router.group(() => {
   router.group(() => {
-    router.post('register', [AuthController, 'register']).as('auth.register');
+    router.post('register', [AuthController, 'register']).as('auth.register').use([middleware.auth() ,middleware.checkRoles(["ROLE_ADMIN"])]);
     router.post('login', [AuthController, 'login']).as('auth.login');
   }).prefix('v1');
 }).prefix('api')
