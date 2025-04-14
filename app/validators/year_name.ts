f805a031e0b8bc3763ddcf75de3fa90e18274462
Alfonso Rafael Solis Rangel <alfonsorafaelsolisrangel@gmail.com>
2025-04-14 09:23:08 -0600

    Validador de actualizar años

--- app/validators/year_name.ts
+++ app/validators/year_name.ts
@@ -6,3 +6,10 @@ export const yearNameValidator = vine.compile(
         year: vine.number().min(2000).max(2100),
     })
 )
+
+export const yearNameUpdateValidator = vine.compile(
+    vine.object({
+        name: vine.string().minLength(2).maxLength(50).optional(),
+        year: vine.number().min(2000).max(2100).optional(),
+    })
+)
