bea1944620395e4f369cb520e6f60ae3cb86fe3b
Alfonso Rafael Solis Rangel <alfonsorafaelsolisrangel@gmail.com>
2025-04-14 13:17:29 -0600

    Migracion de las dependencias

---
+++ database/migrations/1744658073124_create_dependencies_table.ts
@@ -0,0 +1,22 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'dependencies'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name', 50).notNullable()
+      table.integer('subcommittee_id').unsigned().notNullable()
+
+      table.foreign('subcommittee_id').references('id').inTable('subcommittees').onDelete('CASCADE').onUpdate('CASCADE')
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
