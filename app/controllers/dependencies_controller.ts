a332c50fe391f1b84688ff5b3a1ea97408d0390b
Alfonso Rafael Solis Rangel <alfonsorafaelsolisrangel@gmail.com>
2025-04-14 13:53:55 -0600

    Metodo de listar todas las dependencias

--- app/controllers/dependencies_controller.ts
+++ app/controllers/dependencies_controller.ts
@@ -9,6 +9,13 @@ export default class DependenciesController {
         return response.created({ message: "Dependencia creada correctamente.", data: dependency });
     }
 
+    async index ({request, response}: HttpContext){
+        const query_params = request.qs();
+        const dependencies = await this.getDependenciesWithPreloads(query_params);
+        if(dependencies.length === 0) return response.notFound({ message: "No se encontraron resultados." });
+        return response.ok({ message: "Información obtenida correctamente.", data: dependencies });
+    }
+
     private async getDependenciesWithPreloads(params: any, id?: number){
         const query = Dependency.query();
         if(id) query.where('id', id);
