7c28f9e13ef552b75556110acbd3bedf2727e18c
Alfonso Rafael Solis Rangel <alfonsorafaelsolisrangel@gmail.com>
2025-04-14 13:13:55 -0600

    Validador de actualizar subcomite

--- app/validators/subbcommitte.ts
+++ app/validators/subbcommitte.ts
@@ -9,3 +9,13 @@ export const subcommitteeValidator = vine.compile(
         })
     })
 )
+
+export const subcommitteeUpdateValidator = vine.compile(
+    vine.object({
+        name: vine.string().minLength(2).maxLength(50).optional(),
+        year_name_id: vine.number().min(1).exists(async (db, value) => {
+            const match = await db.from('year_names').select('id').where('id', value).first()
+            return !!match
+        }).optional()
+    })
+)
