07d02754843f5b896e210a50d0604926d028edef
Alfonso Rafael Solis Rangel <alfonsorafaelsolisrangel@gmail.com>
2025-04-29 08:45:11 -0600

    Cambio en el nombre de la tabla

--- database/migrations/1745937240722_create_subcommittees_dependencies_table.ts
+++ database/migrations/1745937240722_create_subcommittees_dependencies_table.ts
@@ -1,7 +1,7 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'subcommittees_dependencies'
+  protected tableName = 'dependency_subcommittee'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
