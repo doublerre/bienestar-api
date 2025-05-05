990b4f0bb28fa0c1c954aaa0fe6ad42e5443ecba
Alfonso Rafael Solis Rangel <alfonsorafaelsolisrangel@gmail.com>
2025-05-05 09:36:11 -0600

    Migracion de ubicaciones

---
+++ database/migrations/1746458911824_create_locations_table.ts
@@ -0,0 +1,22 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'locations'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.text('name').notNullable()
+      table.integer('activity_id').unsigned().notNullable()
+
+      table.foreign('activity_id').references('id').inTable('activities').onDelete('CASCADE').onUpdate('CASCADE')
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
